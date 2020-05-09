e6a4ba8971bb61a9e931edc320a697e022668063
Athibet Prawane <atb@odds.team>
2020-05-09 22:15:41 +0700

    Commit cache :bee:

---
+++ ep1/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <span :title="message">
+      Hover your mouse over me for a few seconds to see my dynamically bound
+      title!
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style></style>
