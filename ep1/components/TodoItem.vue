5cec492643f4031c518bbdcf6635b8897198f522
Athibet Prawane <atb@odds.team>
2020-05-16 23:04:47 +0700

    Composing with components :bee:

---
+++ ep1/components/TodoItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>Please say {{ item.text }}</li>
+</template>
+
+<script>
+export default {
+  props: ['item'],
+  mounted() {
+    console.log(JSON.stringify(this.item))
+  }
+}
+</script>
+
+<style lang="css" scoped></style>
--- ep1/pages/index.vue
+++ ep1/pages/index.vue
@@ -1,22 +1,24 @@
-<template>
+<template lang="html">
   <div>
-    <span>{{ message }}</span>
-    <br />
-    <input type="number" v-model.number="message" />
-    <p>{{ selected }}</p>
-    <select v-model="selected">
-      <option value="1">1</option>
-      <option value="2">2</option>
-    </select>
+    <ol>
+      <TodoItem v-for="todo in todos" :key="todo.id" :item="todo"></TodoItem>
+    </ol>
   </div>
 </template>
 
 <script>
+import TodoItem from '@/components/TodoItem'
 export default {
+  components: {
+    TodoItem
+  },
   data() {
     return {
-      message: 'Hello Vue!',
-      selected: null
+      todos: [
+        { id: 1, text: 'hello vue' },
+        { id: 2, text: 'hello react' },
+        { id: 3, text: 'hello angular' }
+      ]
     }
   }
 }
